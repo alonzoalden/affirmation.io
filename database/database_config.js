9649f607215953b8f9779e5c497dbfdfc6df2201
David Flowers <dvdwdflwrs@gmail.com>
2017-01-04 15:45:13 -0600

    Create Post Model

--- database/database_config.js
+++ database/database_config.js
@@ -17,3 +17,12 @@ const User = dbConnection.define('user', {
   name: { type: Sequelize.STRING, unique: true, validate: { notEmpty: true } },
   avatar: { type: Sequelize.STRING },
 });
+
+const Post = dbConnection.define('post', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
+  title: { type: Sequelize.STRING(70), validate: { notEmpty: true } },
+  message: { type: Sequelize.TEXT, validate: { notEmpty: true } },
+  sentiment: { type: Sequelize.INTEGER, allowNull: false },
+  flag: { type: Sequelize.INTEGER, allowNull: false },
+  anon: { type: Sequelize.BOOLEAN, allowNull: false },
+});
