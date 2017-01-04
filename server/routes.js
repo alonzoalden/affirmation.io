0d78c38ca972524083c9ed4dba54d2671ce436e6
Alonzo Alden <alonzoalden@gmail.com>
2017-01-04 15:05:29 -0800

    Added server routes / api endpoints

--- server/routes.js
+++ server/routes.js
@@ -1,20 +1,13 @@
-var morgan = require('morgan'); //<-- debugging
-var bodyParser = require('body-parser');
-var path = require('path');
-var userController = require('./api/users/userController');
-var postController = require('./api/posts/eventController');
+const path = require('path');
+const userController = require('./api/users/userController');
+const postController = require('./api/posts/eventController');
 
 module.exports = function (app, express) {
-  app.use(morgan('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded());
-
-  app.use(express.static(path.join(__dirname, '../client')));
 
   //initiatie express router
   var router = express.Router();
 
-  //these are the api endpoints and routes.
+  //these are the api endpoints and routes
   router.route('/posts/:phase/')
     .post(postController.addAPost)
     .get(postController.getAllPosts);
@@ -24,11 +17,15 @@ module.exports = function (app, express) {
     .put(postController.updateAPost)
     .delete(postController.deleteAPost);
 
-  router.route('/users')
+  router.route('/users/')
     .post(userController.addAUser)
-    .get(userController.getAllUsers)
+    .get(userController.getAllUsers);
+
+  router.route('/users/:id')
+    .get(userController.getAUser)
     .put(userController.updateAUser)
     .delete(userController.deleteAUser);
+
 
   app.use('/api', router);
 
