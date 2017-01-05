a71ff07b004a33dcd03315062b92f459e631d912
Luke Golden <webseedsonline@gmail.com>
2017-01-04 16:39:59 -0800

    Update models and postController with phase, helpful, and unhelpful

--- database/database_config.js
+++ database/database_config.js
@@ -20,9 +20,12 @@ const User = dbConnection.define('user', {
 
 const Post = dbConnection.define('post', {
   id: { type: Sequelize.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
+  phase: { type: Sequelize.STRING(15), validate: { notEmpty: true } },
   title: { type: Sequelize.STRING(70), validate: { notEmpty: true } },
   message: { type: Sequelize.TEXT, validate: { notEmpty: true } },
   sentiment: { type: Sequelize.INTEGER, allowNull: false },
+  helpful: { type: Sequelize.INTEGER, allowNull: false },
+  unhelpful: { type: Sequelize.INTEGER, allowNull: false },
   flag: { type: Sequelize.INTEGER, allowNull: false },
   anon: { type: Sequelize.BOOLEAN, allowNull: false },
 });
