adb4957fce6e65227a2e05d22fb222e7ce436fe9
Luke Golden <webseedsonline@gmail.com>
2017-01-04 21:24:22 -0800

    Updated Dashboard and Created Dashboard Phases

---
+++ src/components/Phase/OnTheJob.js
@@ -0,0 +1,63 @@
+/**
+*
+* Phase
+*
+*/
+
+import React from 'react';
+import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router';
+
+class OnTheJob extends React.Component {
+  render() {
+    const floatLeft = {
+      float: 'left',
+    };
+    const paperStyle = {
+      height: 400,
+      width: 300,
+      margin: 20,
+      overflow: 'auto',
+    };
+    const center = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
+    return (
+      <div style={floatLeft}>
+        <h2 style={center}>I am on the job</h2>
+        <Paper style={paperStyle} zDepth={4}>
+          <div style={{ margin: 20 }}>
+            <h3 style={{ 'font-style': 'italic' }}>Random Affirmation</h3>
+            <div>
+              <h4>The Best Advice Ever</h4>
+              <p>
+                Code. Code. Code. Code. Code. Code.
+                Code. Code. Code. Code. Code. Code.
+              </p>
+              <Link to="/OnTheJob">
+                <FlatButton
+                  label="Read More"
+                  primary={true}
+                />
+              </Link>
+            </div>
+            <div style={center}>
+              <Link to="/OnTheJob">
+                <RaisedButton
+                  label="View All Affirmations"
+                  primary={true}
+                />
+              </Link>
+            </div>
+          </div>
+        </ Paper>
+      </div>
+    );
+  }
+}
+
+export default OnTheJob;
