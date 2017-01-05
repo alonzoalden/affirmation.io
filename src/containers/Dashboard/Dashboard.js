183d97e1560dea8d9a90f55c9b50e4b6b954c300
Luke Golden <webseedsonline@gmail.com>
2017-01-04 19:56:49 -0800

    Create Phase component and Dashboard Container

---
+++ src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+/*
+ *
+ * Dashboard
+ *
+ */
+
+import React from 'react';
+import Phase from '../../components/Phase/Phase';
+
+export class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <Phase />
+        <Phase />
+        <Phase />
+        <Phase />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
