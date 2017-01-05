1947e351e28625c30329cc04cf6007893fb65d21
Alonzo Alden <alonzoalden@gmail.com>
2017-01-04 23:05:40 -0800

    updated posts model with name and avatar, more progress on post preview

--- src/components/PostPreview/PostPreview.js
+++ src/components/PostPreview/PostPreview.js
@@ -12,9 +12,7 @@ import Badge from 'material-ui/Badge';
 import SentimentVerySatisfied from 'material-ui/svg-icons/social/sentiment-very-satisfied';
 import SentimentVeryDissatisfied from 'material-ui/svg-icons/social/sentiment-very-dissatisfied';
 
-
-
-class PostPreview extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class PostPreview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -27,23 +25,30 @@ class PostPreview extends React.Component { // eslint-disable-line react/prefer-
   }
 
   getPreviewPosts() {
-    axios.get('http://localhost:3000/')
+    let phase = this.props.location.pathname.toLowerCase()
+    return axios.get('http://localhost:8000/api/posts' + phase)
+      .then((arr) => {
+          this.setState({ posts: arr.data})
+          console.log(this.props)
+      })
+
   }
 
-  isHelpful() {
+  isHelpful(num) {
     return (
       <Badge
-        badgeContent={5}
+        badgeContent={num}
         primary={true}
       >
         <SentimentVerySatisfied />
       </Badge>
     );
   }
-  isUnhelpful() {
+
+  isUnhelpful(num) {
     return (
       <Badge
-        badgeContent={5}
+        badgeContent={num}
         primary={true}
       >
         <SentimentVeryDissatisfied />
@@ -54,7 +59,7 @@ class PostPreview extends React.Component { // eslint-disable-line react/prefer-
   render() {
     const cardStyle = {
       width: 600,
-      margin: 20,
+      margin: 10,
       overflow: 'auto',
     };
     const center = {
@@ -62,41 +67,38 @@ class PostPreview extends React.Component { // eslint-disable-line react/prefer-
       alignItems: 'center',
       justifyContent: 'center',
     };
+
     return (
       <div>
-        <div style={center}>
-          <div>
-            <div style={{ margin: 20 }}>
-              <Card
-                children={this.isHelpful, this.isUnhelpful}
-                style={cardStyle}
-              >
-                <CardHeader
-                  title="Joe Shmo"
-                  subtitle="Hack Reactor - San Francisco, CA"
-                  avatar="https://s-media-cache-ak0.pinimg.com/564x/4d/b7/b7/4db7b7ecb39c4eebc5b8f5358773e4a2.jpg"
-                />
-                <CardTitle title="Card title" />
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                  Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                  Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                </CardText>
-                <div style={{ float: "right", marginRight: 20 }}> {this.isHelpful()} </div>
-              </Card>
+        {this.state.posts.map((post, index) => {
+          let italicMessage = post.message.slice(0, 50)
+          let message = post.message.slice(50, 200) + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit." + '.....'
+          return (
+            <div style={center}>
+              <div>
+                <div style={{ margin: 10 }}>
+                  <Card
+                    children={this.isHelpful, this.isUnhelpful}
+                    style={cardStyle}
+                  >
+                    <CardHeader
+                      title="Joe Schmo"
+                      subtitle="Hack Reactor - San Francisco, CA"
+                      avatar="https://s-media-cache-ak0.pinimg.com/564x/4d/b7/b7/4db7b7ecb39c4eebc5b8f5358773e4a2.jpg"
+                    />
+                    <CardTitle title={post.title} />
+                    <CardText>
+                      <i><strong>{italicMessage}</strong></i>{message}
+                    </CardText>
+                    <div style={{ float: "right", marginRight: 20 }}> {this.isHelpful(post.sentiment)} {this.isUnhelpful(post.unhelpful)} </div>
+                  </Card>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          )})
+        }
       </div>
-    );
+    )
   }
 }
 
