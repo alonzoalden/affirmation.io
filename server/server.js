5b3368257530b1f11201f8dea4220b4a4bd86bb3
David Flowers <dvdwdflwrs@gmail.com>
2017-01-04 21:43:13 -0600

    Change order of server middleware to correct CORS issues

--- server/server.js
+++ server/server.js
@@ -2,21 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes');
 
 app.use(express.static(path.join(__dirname, '../dist')));
-const router = require('./routes')(app, express);
-
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 app.options('*', cors());
 app.del('*', cors());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app, express);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
