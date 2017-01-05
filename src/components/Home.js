f7cdf8275d06a5a5c27eb40165c2bd512e1517a0
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-01-05 01:28:53 -0500

    Splash page mods.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,88 @@
 import React, {Component} from 'react';
 import {connectProfile} from '../auth';
-import {Link} from 'react-router';
 import './Home.css';
-
+import './Animate.css';
 class Home extends Component {
   static propTypes = {
     ...connectProfile.PropTypes
   };
 
   render() {
-
+    const text = {
+      marginTop: 140
+    };
+    const icon = {
+      height: 120,
+      width: 120,
+      marginTop: 180
+    };
     return (
-      <div className="Home">
-        <div className="Home-intro">
-          <h2>Welcome! You've now got a React app protected by Auth0.</h2>
-          <p>Explore your <Link to="/profile/edit">profile</Link> or check out the <a href="https://auth0.com/docs/quickstart/spa/react">Auth0 docs</a> for more info.</p>
+      <div>
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="alert alert-info" role="alert">
+            <div className="col-xs-4 col-xs-offset-2">
+              <h1 style={text}>'Want to learn?'</h1>
+              <h3>Lorem ipsum dolor sit amet, mel tacimates mandamus ea, minim dicunt
+                mea ne, ei sit principes elaboraret. Purto principes per et, affert feugait
+                eu mea. Id mel suas unum definitiones, veritus lucilius an sea.
+              </h3>
+            </div>
+            <div className="col-xs-3 col-xs-offset-1">
+              <img className='tossing' style={icon} src={require('./rocket.png')}/>
+            </div>
+          </div>
         </div>
       </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="alert alert-success" role="alert">
+            <div className="col-xs-3 col-xs-offset-2">
+              <img className='tossing' style={icon} src={require('./pencil.png')}/>
+            </div>
+            <div className="col-xs-4 col-xs-offset-1">
+              <h1 style={text}>'Learning to code?'</h1>
+                <h3>Lorem ipsum dolor sit amet, mel tacimates mandamus ea, minim dicunt
+                  mea ne, ei sit principes elaboraret. Purto principes per et, affert feugait
+                  eu mea. Id mel suas unum definitiones, veritus lucilius an sea.
+                </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="alert alert-info" role="alert">
+            <div className="col-xs-4 col-xs-offset-2">
+              <h1 style={text}>'Job hunting?'</h1>
+                <h3>Lorem ipsum dolor sit amet, mel tacimates mandamus ea, minim dicunt
+                  mea ne, ei sit principes elaboraret. Purto principes per et, affert feugait
+                  eu mea. Id mel suas unum definitiones, veritus lucilius an sea.
+                </h3>
+            </div>
+            <div className="col-xs-3 col-xs-offset-1">
+              <img className='tossing' style={icon} src={require('./briefcase.png')}/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="alert alert-success" role="alert">
+            <div className="col-xs-3 col-xs-offset-2">
+              <img className='tossing' style={icon} src={require('./molecule.png')}/>
+            </div>
+            <div className="col-xs-4 col-xs-offset-1">
+              <h1 style={text}>'On the job?'</h1>
+                <h3>Lorem ipsum dolor sit amet, mel tacimates mandamus ea, minim dicunt
+                  mea ne, ei sit principes elaboraret. Purto principes per et, affert feugait
+                  eu mea. Id mel suas unum definitiones, veritus lucilius an sea.
+                </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     );
   }
 }
