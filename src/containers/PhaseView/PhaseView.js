cb9bc9643520875b6c6c5c79880a7de75bbe5b91
Alonzo Alden <alonzoalden@gmail.com>
2017-01-06 17:29:00 -0800

    Updates to phase view

--- src/containers/PhaseView/PhaseView.js
+++ src/containers/PhaseView/PhaseView.js
@@ -11,6 +11,7 @@ import FlatButton from 'material-ui/FlatButton';
 import Badge from 'material-ui/Badge';
 import SentimentVerySatisfied from 'material-ui/svg-icons/social/sentiment-very-satisfied';
 import SentimentVeryDissatisfied from 'material-ui/svg-icons/social/sentiment-very-dissatisfied';
+import PostPreview from '../../components/PostPreview/PostPreview';
 
 class PhaseView extends React.Component {
   constructor(props) {
@@ -97,7 +98,7 @@ class PhaseView extends React.Component {
     let phase1 = this.props.location.pathname + '/'
     return (
       <div>
-        {this.state.posts.map((post, index) => {
+        {that.state.posts.map((post, index) => {
 
           let italicMessage = post.message.slice(0, 50)
           let message = post.message.slice(50, 200) + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit." + '...'
