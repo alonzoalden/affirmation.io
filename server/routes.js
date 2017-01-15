5d2ad732948d00f1107981f35406e4755bdd572b
David Flowers <dvdwdflwrs@gmail.com>
2017-01-14 18:01:00 -0600

    Update root route

--- server/routes.js
+++ server/routes.js
@@ -42,7 +42,7 @@ app.use('/api', router);
 
   //need to add this to handle direct addressing of routes.
   //will serve index.html which has our js linked for routing.
-  app.get('/', function (request, response){
+  app.get('/*', function (request, response){
     response.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 };
