4ffd0a4e397bf3da696fb7dbecfc8bb556f8a08e
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-01-19 22:53:44 -0500

    Fix dashboard viewheight and viewwidth.

--- src/components/Phases/Phases.js
+++ src/components/Phases/Phases.js
@@ -10,15 +10,29 @@ import './Phases.css';
 import '../Animate.css';
 
 class Phases extends React.Component {
+  componentDidMount(){
+    window.reload();
+  }
   render() {
+    const center = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
+    const flexbox = {
+      'display': '-webkit-flex',
+      'display': '-ms-flexbox',
+      'display': 'flex',
+      'overflow': 'hidden'
+    };
     return (
-        <div>
-          <div className="row">
-            <div className="col-xs-12">
-              <div className="col-xs-6">
-              <div class="container">
-                <div class="row">
-                    <a href="javascript:">
+
+      <div>
+        <div className={flexbox}>
+          <Link to="/WantToLearn">
+            <div className="col-xs-12 col-md-6 bg1" style={{ 'height': '46vh' }}>
+              <div style={center}>
+                <a href="javascript:">
                       <div className="icon-box-01 bg1 col-sm-5ths col-xs-6">
                         <div className="icon-wrapper">
                           <div className="livicon-evo" data-options="
@@ -27,19 +41,19 @@ class Phases extends React.Component {
                               strokeColor: #ffffff; 
                               strokeColorAction: #fff; 
                               colorsOnHover: custom; 
-                              eventOn: grandparent; 
-                              drawOnViewport: true 
+                              eventOn: self; 
+                              drawOnViewport: false 
                           "></div>
                           <h5 className="text-center text-uppercase white">I WANT TO LEARN TO CODE</h5>
                           </div>
                         </div>
                       </a>
-                  </div>
-                </div>                      
-              </div>
-              <div className="col-xs-6">
-              <div class="container">
-                <div class="row">
+                </div>
+            </div>
+          </Link>
+          <Link to="/LearningToCode">
+            <div className="col-xs-12 col-md-6 bg2" style={{ 'height': '46vh' }}>
+              <div style={center}>
                     <a href="javascript:">
                       <div className="icon-box-01 bg2 col-sm-5ths col-xs-6">
                           <div className="icon-wrapper">
@@ -49,25 +63,21 @@ class Phases extends React.Component {
                                 strokeColor: #ffffff; 
                                 strokeColorAction: #fff; 
                                 colorsOnHover: custom; 
-                                eventOn: grandparent; 
+                                eventOn: self; 
                                 drawOnViewport: true 
                             "></div>
                             <h5 className="text-center text-uppercase white">I AM LEARNING TO CODE</h5>
                         </div>
                       </div>
-                    </a>
-                  </div>
-                </div>                    
-              </div>
+                    </a>              
+                </div>
             </div>
-          </div>
-        <div className="row">
-          <div className="col-xs-12">
-              <div className="col-xs-6">
-                <div class="container">
-                  <div class="row">                 
-                    <a href="javascript:">
-                      <div className="icon-box-01 bg3 col-sm-5ths col-xs-6">
+          </Link>
+          <Link to="/JobHunt">
+            <div className="col-xs-12 col-md-6 bg3" style={{ 'height': '5vh' }}>
+              <div style={center}>
+                  <a href="javascript:">
+                      <div className="icon-box-01 bg3 col-sm-5ths col-xs-6" style={{ 'height': '45vh' }}>
                         <div className="icon-wrapper">
                           <div className="livicon-evo" data-options="
                               name: desktop.svg; 
@@ -75,21 +85,21 @@ class Phases extends React.Component {
                               strokeColor: #ffffff; 
                               strokeColorAction: #fff; 
                               colorsOnHover: custom; 
-                              eventOn: grandparent; 
+                              eventOn: self; 
                               drawOnViewport: true 
                           "></div>
                           <h5 className="text-center text-uppercase white">I AM ON THE JOB HUNT</h5>
                         </div>
                       </div>
                     </a>
-                  </div>
-                </div>                  
-              </div>
-              <div className="col-xs-6">
-              <div class="container">
-                <div class="row">                 
-                  <a href="javascript:">
-                    <div className="icon-box-01 bg1 col-sm-5ths col-xs-6">
+                </div>
+            </div>
+          </Link>
+          <Link to="/OnTheJob">
+            <div className="col-xs-12 col-md-6 bg1" style={{ 'height': '5vh' }}>
+              <div style={center}>
+                <a href="javascript:">
+                    <div className="icon-box-01 bg1 col-sm-5ths col-xs-6" style={{ 'height': '45vh' }}>
                       <div className="icon-wrapper">
                         <div className="livicon-evo" data-options="
                             name: paper-plane.svg; 
@@ -97,17 +107,16 @@ class Phases extends React.Component {
                             strokeColor: #ffffff; 
                             strokeColorAction: #fff; 
                             colorsOnHover: custom; 
-                            eventOn: grandparent; 
+                            eventOn: self; 
                             drawOnViewport: true 
                         "></div>
                         <h5 className="text-center text-uppercase white">I AM ON THE JOB</h5>
                       </div>
                     </div>
                   </a>
-                </div>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     );
